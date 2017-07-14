e84cc1f669d8b5383b7cd7ff39053c89d1950f28
Suhyun Lee <ilyf28@gmail.com>
2017-07-14 16:34:54 +0900

    Opening a connection

---
+++ cookbook/2_1_opening_a_connection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from main import *
+
+# login to DSM instance
+# declare and define the payload variable
+payload = {}
+
+# define the REST API call
+REST = '/ApiConnection/Login'
+
+# build the complete REST API URL
+completeURL = '%s%s' % (baseURL, REST if REST[0] != '/' else REST[1:])
+
+# execute REST API call via the HTTP POST method
+print connection.post(completeURL, data=json.dumps(payload, ensure_ascii=False).encode('utf-8'), headers=header, verify=verify_cert)
